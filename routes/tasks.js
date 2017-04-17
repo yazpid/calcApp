e9fb6a26d5351d7e439a9dd023b98709e9ca4b9f
Łukasz-Komputer\Łukasz <yazpid@gmail.com>
2017-04-17 22:58:54 +0200

    boilerplate server

---
+++ routes/tasks.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+
+var url = 'mongodb://localhost:27017/calcApp';
+var collection = ['tasks'];
+var db = mongojs(url, collection);
+
+//get all tasks
+router.get('/tasks', function (req, res, next) {
+  db.tasks.find(function (err, tasks) {
+      if(err){
+          res.send(err);
+      }
+      res.json(tasks);
+  })
+});
+
+//get single task
+router.get('/task/:id', function (req, res, next) {
+    db.tasks.findOne({_id : mongojs.ObjectId(req.params.id)},function (err, task) {
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    })
+});
+
+//save single task
+router.post('/task', function (req, res , next) {
+    var task = req.body;
+    if(!task.title || (task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error" : "Bad data"
+        });
+    }
+    else
+    {
+        db.tasks.save(task, function (err, task) {
+            if(err){
+                res.send(err)
+            }
+            res.json(task);
+        })
+    }
+});
+
+
+//delete single task
+router.delete('/task/:id', function (req, res, next) {
+    db.tasks.remove({_id : mongojs.ObjectId(req.params.id)},function (err, task) {
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    })
+});
+
+// update task
+
+router.put('/task/:id', function (req, res, next) {
+    var task = req.body;
+    var updTask = {};
+
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
+    if(task.title){
+        updTask.title = task.title;
+    }
+
+    if(!updTask){
+        res.status(400);
+        res.json({
+            "error" : "Bad data"
+        })
+    }else
+    {
+        db.tasks.update({_id : mongojs.ObjectId(req.params.id)},updTask, {},function (err, task) {
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        })
+    }
+
+
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var port = 3000;
+
+var app = express();
+
+//view engine
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// set static folder
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+//body parser
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(port, function () {
+    console.log("server start on "+port);
+});
